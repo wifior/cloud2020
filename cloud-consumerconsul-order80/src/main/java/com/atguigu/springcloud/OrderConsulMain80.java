a783df7ee5c31de09a04ce31f441c81757db7fe6
葛会军 <bgenius@sina.com>
2020-12-09 21:55:39 +0800

    服务消费者注册进Consul

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName OrderMain80
+ * @Description TODO
+ * @Author Think
+ * @Date 2020/12/1 19:07
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName ApplicationContextConfig
+ * @Description TODO
+ * @Author Think
+ * @Date 2020/12/1 19:18
+ * @Version 1.0
+ **/
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    @LoadBalanced  //使用loadbalanced注解赋予resttemplate负载均衡的能力
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
