cb1882f90f5b1aa88dd5ab7130bb2cb4526f4444
葛会军 <bgenius@sina.com>
2020-12-09 20:52:51 +0800

    80订单注册到zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZK80.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName OrderZK80
+ * @Description TODO
+ * @Author Think
+ * @Date 2020/12/9 20:45
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZK80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZK80.class,args);
+    }
+}
